813bee1cd4e7f75dcc2fdcf6b4ec9cd3e4b6a586
Rashid Siddiqui <rsh_bcs@hotmail.com>
2020-12-23 01:49:24 +0500

    add secure endpoint

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -24,20 +24,20 @@ namespace SecureAPI.Controllers
             _logger = logger;
         }
 
-        //[Authorize]
-        //[HttpGet]
-        //[Route("/")]
-        //public IEnumerable<WeatherForecast> Get()
-        //{
-        //    var rng = new Random();
-        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        //    {
-        //        Date = DateTime.Now.AddDays(index),
-        //        TemperatureC = rng.Next(-20, 55),
-        //        Summary = Summaries[rng.Next(Summaries.Length)]
-        //    })
-        //    .ToArray();
-        //}
+        [Authorize]
+        [HttpGet]
+        [Route("/getsecure")]
+        public IEnumerable<WeatherForecast> GetSecure()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
 
         [HttpGet]
         [Route("/")]
